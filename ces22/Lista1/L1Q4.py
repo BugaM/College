ebc2ad8fa1e1aa33b9a600e803e2471772e90d58
buga <marcelo.buga@hotmail.com>
2022-03-14 21:46:39 -0300

    improving function complexity

--- ces22/Lista1/L1Q4.py
+++ ces22/Lista1/L1Q4.py
@@ -1,14 +1,16 @@
+from math import sqrt
+
 def is_prime(n):
     """Function that recognizes if the integer n is prime"""
     prime = True
     if n < 2:
         prime = False
     else:
-        for i in range(2, n):
+        for i in range(2, int(sqrt(n))):
             if n % i == 0:
                 prime = False
     return prime
 
 
 if __name__ == "__main__":  # main funtion for testing
-    print(is_prime(107))
+    print(is_prime(253))
